a824c2b8dac06574c79dfec612cda7ba7f07ee01
Jason Ranjit <jasonranjit@gmail.com>
2026-08-19 00:34:11 -0500

    testbench top module

---
+++ Counter/tb/tb_top.sv
@@ -0,0 +1,33 @@
+`include "interface.sv"
+`include "test.sv"
+module testbench();
+  
+  bit clk;
+  bit rst;
+  
+  initial begin
+    clk = 0;
+    forever #10 clk = ~clk;
+  end
+  
+  intf intff(clk,rst);
+  test t(intff);
+  
+  counter DUT(.load(intff.load),
+              .enable(intff.enable),
+              .rst(intff.rst),
+              .clk(intff.clk),
+              .count_val(intff.count_val),
+              .count(intff.count)
+             );
+  
+  initial begin
+    rst = 1;
+    @(intff.cb);
+    rst = 0;
+    $dumpfile("image.vcd");
+    $dumpvars();
+    #200
+    $finish();
+  end
+endmodule
